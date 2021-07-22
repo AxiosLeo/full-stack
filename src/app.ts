42bbec2b4315a50b85434491c45e95cc14cf5fbc
axios <axiosleo@foxmail.com>
2021-07-22 19:03:01 +0800

    feat: throw not found http error on end step

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,10 @@
 /**
  * route require elements : <module-name>/<controller-name>/<action-name>
  */
-import { KoaContext } from './types';
+import { StatusCode, KoaContext, RESTfulHttpMethod } from './types';
 import * as modules from './modules';
 import { getRouteInfo } from './core/routes';
+import { HttpError } from './core/response';
 
 modules.loadModules();
 
@@ -14,6 +15,13 @@ import * as events from './core/events';
  */
 const begin = async (context: KoaContext): Promise<void> => {
   await events.trigger('app-begin', context);
+  const router = getRouteInfo(context.url, context.app.req.method as RESTfulHttpMethod);
+  console.log(router);
+  if (router) {
+    context.router = router;
+  } else {
+    throw new HttpError(StatusCode.notFound, 404);
+  }
 };
 
 /**
@@ -25,8 +33,6 @@ const middleware = async (context: KoaContext): Promise<void> => {
   await events.trigger('app-middleware', context);
 
   // exec middleware by routes configuration
-  // middlewares.dispatch(context.method, context.url);
-  console.log(getRouteInfo(context.url, context.app.req.method));
 };
 
 /**
@@ -45,9 +51,14 @@ const controller = async (context: KoaContext): Promise<void> => {
   await events.trigger('app-controller', context);
 };
 
+const end = async (): Promise<void> => {
+  throw new HttpError(StatusCode.notFound, 404);
+};
+
 export {
   begin,
   middleware,
   validate,
-  controller
+  controller,
+  end
 };
