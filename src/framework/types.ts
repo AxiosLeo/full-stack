b559edd87252e63514d5fcf307ce0a59630718d0
axios <axiosleo@foxmail.com>
2021-08-02 11:15:40 +0800

    chore: remove response status code statement

--- src/framework/types.ts
+++ src/framework/types.ts
@@ -53,23 +53,3 @@ export interface AppConfiguration {
   validator: Array<any>,
   routes: Router[],
 }
-
-export type HttpSucceededCode = 200 | 201;
-export type HttpErrorStatusCode = 400 | 401 | 403 | 404 | 409 | 500;
-export type HttpStatusCode = HttpSucceededCode | HttpErrorStatusCode;
-export type HttpResponseFormat = 'json' | 'xml' | 'raw' | 'jsonp';
-
-/**
- * status code for request
- * @example <status-code>;<code-message>
- */
-export enum StatusCode {
-  // common code
-  unknown = '000;Unknown Error',
-  success = '200;Success',
-  notFound = '404;Not Found',
-  error = '500;Internal Error',
-
-  // module code : 1
-  notFoundDataFile = '1-404;Not Found Data File'
-}
