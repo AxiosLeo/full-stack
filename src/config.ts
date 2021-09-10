5db67fb9408991b6b826e343c1f4042dd216c503
axios <axiosleo@foxmail.com>
2021-09-10 23:46:29 +0800

    feat: add config

---
+++ src/config.ts
@@ -0,0 +1,13 @@
+import { Configuration } from '@axiosleo/cli-tool';
+
+const config = new Configuration({
+  db: {
+    // mysql: {
+    //   username: 'root',
+    //   password: 'root',
+    //   port: 3306,
+    //   host: 'localhost'
+    // },
+  }
+});
+export default config;
