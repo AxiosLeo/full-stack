74e17aff4765580847a97b78177d30929e762cab
axios <axiosleo@foxmail.com>
2021-09-20 11:27:44 +0800

    feat: mysql connector class

--- src/connector/mysql.ts
+++ src/connector/mysql.ts
@@ -1,11 +1,40 @@
 // https://github.com/mysqljs/mysql
 import mysql from 'mysql';
-import config from '../config';
 
-const clients: { [k: string]: mysql.Connection } = {};
+class MysqlConnector {
+  clients: { [k: string]: mysql.Connection } = {};
+  default: mysql.Connection | undefined;
 
-if (config.has('db.mysql')) {
-  clients['default'] = mysql.createConnection(config.get('db.mysql'));
+  newConnection(name: string, dbconfig: string | mysql.ConnectionConfig): mysql.Connection {
+    this.clients[name] = mysql.createConnection(dbconfig);
+    if (!this.default) {
+      this.default = this.clients[name];
+    }
+    return this.clients[name];
+  }
+
+  select(name: string | undefined): mysql.Connection {
+    if (!name && this.default) {
+      return this.default;
+    }
+    if (!name) {
+      throw new Error('Please initialize Mysql DB connectors');
+    }
+    return this.clients[name];
+  }
+
+  initialize(dbconfig: Record<string, any>) {
+    Object.keys(dbconfig).forEach((name: string) => {
+      const dbsettings = dbconfig[name];
+      switch (dbsettings['type']) {
+      case 'mysql':
+        this.newConnection(name, dbsettings);
+        break;
+      default:
+        return;
+      }
+    });
+  }
 }
 
-export default clients['default'];
+export default new MysqlConnector();
