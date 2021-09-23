9a7ba7cb877c633242a0c9467ce2f63f8736ce02
axios <axiosleo@foxmail.com>
2021-09-23 21:00:10 +0800

    feat: redis connector class

--- src/connector/redis.ts
+++ src/connector/redis.ts
@@ -1,17 +1,40 @@
 // https://www.npmjs.com/package/redis
 import redis from 'redis';
 
-const clients: { [k: string]: redis.RedisClient } = {};
+class RedisConnector {
+  clients: { [k: string]: redis.RedisClient } = {};
+  default: redis.RedisClient | undefined;
 
-const getClient = (name: string): redis.RedisClient => {
-  if (!clients[name]) {
-    throw new Error(`Redis Client ${name} not exist`);
+  newConnection(name: string, dbconfig: redis.ClientOpts): redis.RedisClient {
+    this.clients[name] = redis.createClient(dbconfig);
+    if (!this.default) {
+      this.default = this.clients[name];
+    }
+    return this.clients[name];
   }
-  return clients[name];
-};
 
-export const initClient = (name: string, options?: redis.ClientOpts): void => {
-  clients[name] = redis.createClient(options);
-};
+  select(name: string | undefined): redis.RedisClient {
+    if (!name && this.default) {
+      return this.default;
+    }
+    if (!name) {
+      throw new Error('Please initialize Mysql DB connectors');
+    }
+    return this.clients[name];
+  }
 
-export default getClient;
+  initialize(dbconfig: Record<string, any>) {
+    Object.keys(dbconfig).forEach((name: string) => {
+      const dbsettings = dbconfig[name];
+      switch (dbsettings['type']) {
+      case 'redis':
+        this.newConnection(name, dbsettings);
+        break;
+      default:
+        return;
+      }
+    });
+  }
+}
+
+export default new RedisConnector();
