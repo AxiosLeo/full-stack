f249cbff1cfcaa94124a61c620050d5e1051f2d2
axios <axiosleo@foxmail.com>
2021-10-27 18:45:02 +0800

    refactor: signature service

--- src/services/signature.ts
+++ src/services/signature.ts
@@ -1,8 +1,4 @@
-import {
-  KoaContext,
-} from '../framework';
-
-import { error } from '../response';
+import Koa from 'koa';
 import CryptoJS = require('crypto-js');
 
 const signHeaders = [
@@ -17,50 +13,38 @@ const signHeaders = [
   'x-timestamp'
 ];
 
-const signatureMethods: Record<string, (str: string, secret: string) => Promise<CryptoJS.lib.WordArray>> = {
+export const signatureMethods: Record<string, (str: string, secret: string) => Promise<CryptoJS.lib.WordArray>> = {
   hmacsha1: async (str: string, secret: string): Promise<CryptoJS.lib.WordArray> => {
     return CryptoJS.HmacSHA1(str, secret);
   },
-  hmansha256: async (str: string, secret: string): Promise<CryptoJS.lib.WordArray> => {
+  hmacsha256: async (str: string, secret: string): Promise<CryptoJS.lib.WordArray> => {
     return CryptoJS.HmacSHA256(str, secret);
   }
 };
 
-const secretKey = 'test';
+export const supportedSignatureMethods = async (signatureMethod: string) => {
+  return signatureMethods[signatureMethod] ? true : false;
+};
 
-export const checkSignature = async (context: KoaContext): Promise<void> => {
-  const headers = context.koa.request.headers;
-  const requestSignature = headers['x-signature'];
-  if (!requestSignature) {
-    error(400, 'Lost X-Signature header');
-  }
-  const requestTimestamp = parseFloat(headers['x-timestamp'] ? headers['x-timestamp'] as string : '');
-  if (!requestTimestamp) {
-    error(400, 'Lost X-Timestamp header');
-  }
-  const nowTimestamp = (new Date()).valueOf();
-  if (Math.abs(nowTimestamp - requestTimestamp) > 10000) {
-    error(400, 'Server timestamp and request timestamp interval greater than 10 seconds. Please check that your NTP service is working properly.');
-  }
-  let signStr = context.koa.request.method + '\n' +
-    context.koa.request.path + '\n' +
+export const makeSignatureString = async (request: Koa.Request) => {
+  const headers = request.headers;
+  const method = request.method;
+  const pathinfo = request.path;
+  const query = request.query;
+
+  let signStr = method + '\n' +
+    pathinfo + '\n' +
     signHeaders.map((str: string) => {
       return `${str}:${headers[str] ? headers[str] : ''}`;
     }).join(',') + '\n';
-  const query = context.koa.request.query;
   signStr = signStr + Object.keys(query).sort().map((key: string) => {
     return `key:${query[key] ? query[key] : ''}`;
   }).join(',') + '\n';
+  return signStr;
+};
 
-  const signatureMethod = !headers['x-signature-method'] ? 'hmacsha1' : headers['x-signature-method'] as string;
-
-  if (signatureMethods[signatureMethod]) {
-    const handler = signatureMethods[signatureMethod];
-    const signature = await handler(signStr, secretKey);
-    if (signature.toString() !== requestSignature) {
-      error(400, `Signature is not matched. string on server to sign is "${signStr}"`);
-    }
-  } else {
-    error(400, `Unsupported signature method: ${signatureMethod}. Only supported ${Object.keys(signatureMethods).join(',')}`);
-  }
+export const makeSignature = async (signStr: string, secretKey: string, signatureMethod: string): Promise<string> => {
+  const handler = signatureMethods[signatureMethod];
+  const signature = await handler(signStr, secretKey);
+  return signature.toString();
 };
